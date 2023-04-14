e199c930be35644e80fe8a0bcce17866bcf68044
Samueal Louis Dsouza <dsouzasamueal07@gmail.com>
2023-04-14 23:21:19 +0530

    davinci working succesfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,26 @@ import sqlLogo from "./assets/sql-logo.png";
 
 function App() {
   const [queryDescription, setQueryDescription] = useState("");
-
-  const sendToBackend = (e) => {
+  const [sqlQuery, setSqlQuery] = useState("");
+  const sendToBackend = async (e) => {
     e.preventDefault();
+
+    const generatedsqlQuery = await generateQuery();
+    setSqlQuery(generatedsqlQuery);
+    // console.log("sent by server to frontend", generateQuery);
+  };
+
+  const generateQuery = async () => {
+    const response = await fetch("http://localhost:3000/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ queryDescription: queryDescription }),
+    });
+
+    const data = await response.json();
+    return data.response.trim();
   };
   return (
     <main className={styles.main}>
@@ -18,10 +35,12 @@ function App() {
           type="text"
           name="qyery-description"
           placeholder="Enter your query Description"
-          onChange={() => setQueryDescription(e.target.value)}
+          onChange={(e) => setQueryDescription(e.target.value)}
         />
 
         <input type="submit" value="Generate Query" />
+
+        <pre>{sqlQuery}</pre>
       </form>
     </main>
   );
